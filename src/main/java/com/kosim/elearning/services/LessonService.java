223d1ce130e5200be0b82f4fd695c4bf99d65ffd
KrystianKosim <krystiankosim123@gmail.com>
2022-05-26 14:13:09 +0200

    Create Service and repository for lessons

---
+++ src/main/java/com/kosim/elearning/services/LessonService.java
@@ -0,0 +1,50 @@
+package com.kosim.elearning.services;
+
+import com.kosim.elearning.models.Lesson;
+import com.kosim.elearning.repsitories.LessonRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class LessonService {
+
+    private final LessonRepository lessonRepository;
+
+    public List<Lesson> getAllLessons() {
+        return lessonRepository.findAll();
+    }
+
+    public Optional<Lesson> getSingleLesson(int lessonId) {
+        return lessonRepository.findById(lessonId);
+    }
+
+    public boolean removeLessonById(int lessonId) {
+        return lessonRepository.removeById(lessonId);
+    }
+
+    public boolean addNewLesson(Lesson lesson) {
+        Optional<Lesson> foundedLesson = lessonRepository.findById(lesson.getLessonId());
+        if (foundedLesson.isPresent()) {
+            return false;
+        }
+        return lessonRepository.addLesson(lesson);
+    }
+
+    public Optional<Lesson> editLesson(int lessonId, Lesson updateLesson) {
+        Optional<Lesson> foundedLesson = lessonRepository.findById(lessonId);
+        if (foundedLesson.isPresent()) {
+            Lesson lesson = foundedLesson.get();
+            lesson.setLessonId(updateLesson.getLessonId());
+            lesson.setDate(updateLesson.getDate());
+            lesson.setTeacherName(updateLesson.getTeacherName());
+            lesson.setStudentName(updateLesson.getStudentName());
+            lesson.setTopic(updateLesson.getTopic());
+        }
+        return foundedLesson;
+    }
+
+}
